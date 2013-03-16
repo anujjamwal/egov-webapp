8274c7fe87b0b018c6bfcdadf67b993170b31115
arnav90 <arnavk90@thoughtworks.com>
2013-03-16 13:03:38 +0530

    +Riya View List of categories as JSON. Need to add seed.

--- app.rb
+++ app.rb
@@ -1,7 +1,21 @@
 #app.rb
 
+require 'sinatra'
+require 'mongoid'
+require_relative('lib/models/category')
+
 set :erb, :format => :html
 
-get "/" do
-	erb :form
-end 
+Mongoid.load!('config/mongoid.yml', :development)
+
+get '/' do
+
+  erb :form
+
+end
+
+get '/categories' do
+  content_type :json
+  all_categories =IncidentCategory.all
+  all_categories.to_json
+end
